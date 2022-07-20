7dd44352c62d36447a7bbd2d88b6a43070d74fc3
Abul Kalam Azad <me.azad99@gmail.com>
2022-07-20 19:22:07 +0600

    react-shopping-list  component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import ShoppingForm from './ShoppingForm';
+
+function App() {
+	return (
+		<div className='App'>
+			<ShoppingForm />
+		</div>
+	);
+}
+
+export default App;
